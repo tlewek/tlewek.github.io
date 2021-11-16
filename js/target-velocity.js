ffe989d9a0162c80fe97e2aab5da0f04d9ce8eca
Tom Lewek <t.j.lewek@gmail.com>
2021-11-15 21:14:42 -0500

    stub out target velocity calculator

---
+++ js/target-velocity.js
@@ -0,0 +1,7 @@
+function targetVelocity {
+  let averageVelocity = document.getElementById("average-velocity").value;
+  let baselineCapacity = document.getElementById("baseline-capacity").value;
+  let adjustedCapacity = document.getElementById("adjusted-capacity").value;
+  document.write(averageVelocity * (adjustedCapacity / baselineCapacity));
+  document.write("<br/><button onClick='location.reload()'>Refresh</button>");
+}

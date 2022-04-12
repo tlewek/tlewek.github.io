ba345d4ed6778a1e34710bddb266d97b0f227b42
Tom Lewek <t.j.lewek@gmail.com>
2022-04-12 16:23:02 -0400

    create rebrander

--- js/names.js
+++ js/names.js
@@ -1,4 +1,10 @@
 const names = [
+  "MLB Tap Experiences",
+  "MLB Swipe Stories Platform",
+  "MLB Stories Presented by Google Cloud Platform",
+  "MLB Stories Presented by Corona, Official Cerveza of Baseball",
+  "MLB Narratives",
+  "MLB Visual Novelas"
 ];
 
 const randomName = names[Math.floor(Math.random()*names.length)];

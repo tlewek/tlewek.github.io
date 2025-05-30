30c05e8e3e3ed4ee8e0fbebb8c47026ee9ceaa54
Tom Lewek <thomas.lewek@mlb.com>
2025-05-30 16:55:44 -0400

    test app with raffle data

--- js/names.js
+++ js/names.js
@@ -1,10 +1,20 @@
 const names = [
-  "MLB Tap Experiences",
-  "MLB Swipe Stories Platform",
-  "MLB Stories Presented by Google Cloud Platform",
-  "MLB Stories Presented by Corona, Official Cerveza of Baseball",
-  "MLB Narratives",
-  "MLB Visual Novelas"
+  {
+    ticketNumber: "76249564A0550",
+    firstName: "Ben",
+    lastName: "Katz",
+    email: "bkinlic@gmail.com",
+    phone: null,
+    ticketType: "Raffle Ticket/Boleto de Rifa: $100 Gift Certificate to Major League Baseball Shop / Certificado de regalo de $100 para la tienda de MLB"
+  },
+  {
+    ticketNumber: "78986927A0545",
+    firstName: "Jaron",
+    lastName: "Yu",
+    email: "jaron7778@yahoo.com",
+    phone: "+13476819981",
+    ticketType: "Raffle Ticket/Boleto de Rifa: $100 Gift Certificate to Major League Baseball Shop / Certificado de regalo de $100 para la tienda de MLB"
+  }
 ];
 
 const randomName = names[Math.floor(Math.random()*names.length)];
